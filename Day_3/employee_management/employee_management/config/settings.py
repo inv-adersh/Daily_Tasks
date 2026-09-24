edb477f136a622a25885a2a776940bac169bd299
inv-adersh <adersh.a@innovaturelabs.com>
2026-09-24 18:58:32 +0530

    refactor: update Django module paths for config package structure

--- Day_3/employee_management/employee_management/config/settings.py
+++ Day_3/employee_management/employee_management/config/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'employee_management.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'employee_management.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
